0b013cb1b3d9af23c447ac41609b6de5944d1e3b
sykes836 <bernard.sykes836@gmail.com>
2018-08-09 20:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,41 @@
 
 function scuberGreetingForFeet(distance) {
   if (distance >= 199 && distance <= 400){
-    return "This one is on me!"
+    return "This one is on me!";
+  }
+  if (distance >= 2001) { 
+    return "I will gladly take your thirty bucks.";
+  }
+    if (distance >= 2501) { 
+    return "No can do.";
   }
 }
+
+
+function ternaryCheckCity (city) {
+   if (city === 'NYC') ? true : false;
+}rn 
+}
+    
+
+  // describe('ternaryCheckCity()', function () {
+  //   it('returns "Ok, sounds good." when the city is NYC', function () {
+  //     expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+  //   });
+
+  //   it('should return "No go." if the destination city is not NYC', function () {
+  //     expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+  //   });
+  // });
+
+  // describe('switchOnCharmFromTip()', function () {
+  //   it('should return "Thank you so much." if the tip is generous', function () {
+  //     expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+  //   });
+
+  //   it('should return "Thank you." if the tip is not as generous', function () {
+  //     expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+  //   });
+
+  //   it('should return "Bye." if anything else', function () {
+  //     expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
